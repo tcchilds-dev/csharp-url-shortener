28cbc71bbffa17bc3e243e88dccbaccca609f764
Thomas Childs <thomas.c.childs@gmail.com>
2026-08-15 20:33:41 +0100

    refactor code generator to be more concise

--- UrlShortener.Api/Helpers/ShortCodeGenerator.cs
+++ UrlShortener.Api/Helpers/ShortCodeGenerator.cs
@@ -4,18 +4,11 @@ namespace UrlShortener.Api.Utilities;
 
 public static class ShortCodeGenerator
 {
-    private const string Alphabet =
+    private const string _choices =
         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     public static string Generate(int length = 7)
     {
-        return string.Create(length, Alphabet, static (characters, alphabet) =>
-        {
-            for (var i = 0; i < characters.Length; i++)
-            {
-                var index = RandomNumberGenerator.GetInt32(alphabet.Length);
-                characters[i] = alphabet[index];
-            }
-        });
+        return RandomNumberGenerator.GetString(_choices, length);
     }
 }
